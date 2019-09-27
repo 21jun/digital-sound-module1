6ed3ffa41b345cebf03b0f2fc696dda5a91986c3
21jun <21jun7654@gmail.com>
2019-09-27 21:05:16 +0900

    helicopter

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import scipy.io.wavfile as wf
 import numpy as np
 from pathlib import Path
+from math import cos,pi
 # 16000hz, 16bit, stereo wav file
-path = Path("music/Post Malone - Die For Me Ft. Halsey, Future.wav")
+# path = Path("music/Post Malone - Die For Me Ft. Halsey, Future.wav")
+path = Path("music/helicopter2.wav")
 
 rate, data = wf.read(path)
 
 left = data[:, 0]
 right = data[:, 1]
-# right.flags : default setting is read-only 
 right.setflags(write=1)
+left.setflags(write=1)
 
-# 오른쪽 체널 1분간 음소거 (16000hz * 60 sec)
-for i in range(16000*60):
-    right[i] = 0
+# 한바퀴 회전
+for i in range(16000*4):
+    right[i] = right[i] * (cos(float(i)/(16000*2) * pi)+1) / 2.0
+    left[i] = left[i] * (cos(float(i)/(16000*2) * pi + pi)+1) / 2.0 
 
 # left, right 순서로 concat
 sum = np.concatenate((left.reshape(-1,1), right.reshape(-1,1)),axis =1)
